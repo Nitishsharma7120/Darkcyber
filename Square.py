7ae30f2973a973eb566a85e0ec9f173f9a513736
Nitishsharma7120 <61767759+Nitishsharma7120@users.noreply.github.com>
2020-03-16 09:28:55 +0530

    Update Square.py

--- Square.py
+++ Square.py
@@ -27,13 +27,4 @@ def square (a):
 	return a*a
 x = input ("enter your square")
 y =(square (int(x)))
-print ("square of number is" + str (y))def square (a):
-	return a*a
-x = input ("enter your square")
-y =(square (int(x)))
-print ("square of number is" + str (y))
-def square (a):
-	return a*a
-x = input ("enter your square")
-y =(square (int(x)))
 print ("square of number is" + str (y))

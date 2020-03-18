0327b5fef137633b8f63c68f2d64e106a8660a3c
Square <61767759+Square5@users.noreply.github.com>
2020-03-18 16:11:56 +0530

    Update Square.py

--- Square.py
+++ Square.py
@@ -29,4 +29,124 @@ y =(square (int(x)))
 print ("square of number is :-" + str (y))
 
 
-print ("thank you") 
+
+def square (a):
+	return a*a
+x = input ("enter your square :-")
+y =(square (int(x)))
+print ("square of number is :-" + str (y))
+
+
+def square (a):
+	return a*a
+x = input ("enter your square :-")
+y =(square (int(x)))
+print ("square of number is :-" + str (y))
+
+def square (a):
+	return a*a
+x = input ("enter your square :-")
+y =(square (int(x)))
+print ("square of number is :-" + str (y))
+
+def square (a):
+	return a*a
+x = input ("enter your square :-")
+y =(square (int(x)))
+print ("square of number is :-" + str (y))
+def square (a):
+	return a*a
+x = input ("enter your square :-")
+y =(square (int(x)))
+print ("square of number is :-" + str (y))
+
+def square (a):
+	return a*a
+x = input ("enter your square :-")
+y =(square (int(x)))
+print ("square of number is :-" + str (y))
+
+def square (a):
+	return a*a
+x = input ("enter your square :-")
+y =(square (int(x)))
+print ("square of number is :-" + str (y))
+def square (a):
+	return a*a
+x = input ("enter your square :-")
+y =(square (int(x)))
+print ("square of number is :-" + str (y))
+def square (a):
+	return a*a
+x = input ("enter your square :-")
+y =(square (int(x)))
+print ("square of number is :-" + str (y))
+
+
+
+
+def square (a):
+	return a*a
+x = input ("enter your square :-")
+y =(square (int(x)))
+print ("square of number is :-" + str (y))
+
+def square (a):
+	return a*a
+x = input ("enter your square :-")
+y =(square (int(x)))
+print ("square of number is :-" + str (y))
+
+
+def square (a):
+	return a*a
+x = input ("enter your square :-")
+y =(square (int(x)))
+print ("square of number is :-" + str (y))
+
+
+def square (a):
+	return a*a
+x = input ("enter your square :-")
+y =(square (int(x)))
+print ("square of number is :-" + str (y))
+
+def square (a):
+	return a*a
+x = input ("enter your square :-")
+y =(square (int(x)))
+print ("square of number is :-" + str (y))
+
+
+def square (a):
+	return a*a
+x = input ("enter your square :-")
+y =(square (int(x)))
+print ("square of number is :-" + str (y))
+
+def square (a):
+	return a*a
+x = input ("enter your square :-")
+y =(square (int(x)))
+print ("square of number is :-" + str (y))
+
+def square (a):
+	return a*a
+x = input ("enter your square :-")
+y =(square (int(x)))
+print ("square of number is :-" + str (y))
+
+
+print ("thank you😊😊😊😊") 
+
+
+
+
+
+
+
+
+
+
+
+

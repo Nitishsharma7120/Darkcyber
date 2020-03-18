02e80931289777c8b5d3b27eede54281756f3d36
Square <61767759+Square5@users.noreply.github.com>
2020-03-18 15:45:35 +0530

    Update Square.py

--- Square.py
+++ Square.py
@@ -1,32 +1,32 @@
 def square (a):
 	return a*a
-x = input ("enter your square")
+x = input ("enter your square :-")
 y =(square (int(x)))
-print ("square of number is" + str (y))
+print ("square of number is :-" + str (y))
 
 def square (a):
 	return a*a
-x = input ("enter your square")
+x = input ("enter your square :-")
 y =(square (int(x)))
-print ("square of number is" + str (y))
+print ("square of number is :-" + str (y))
 
 def square (a):
 	return a*a
-x = input ("enter your square")
+x = input ("enter your square :-")
 y =(square (int(x)))
-print ("square of number is" + str (y))
+print ("square of number is :-" + str (y))
 
 def square (a):
 	return a*a
-x = input ("enter your square")
+x = input ("enter your square :-")
 y =(square (int(x)))
-print ("square of number is" + str (y))
+print ("square of number is :-" + str (y))
 
 def square (a):
 	return a*a
-x = input ("enter your square")
+x = input ("enter your square :-")
 y =(square (int(x)))
-print ("square of number is" + str (y))
+print ("square of number is :-" + str (y))
 
 
 print ("thank you") 
